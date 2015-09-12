4e1fd67aa0a1af077c1a2d27b025a166d86ea462
Sasikanth <dronavalli.sasi@gmail.com>
2015-09-12 21:36:31 +0530

    animated NO

--- src/ios/DocumentHandler.m
+++ src/ios/DocumentHandler.m
@@ -6,12 +6,12 @@
 - (void)HandleDocumentWithURL:(CDVInvokedUrlCommand*)command;
 {
     __weak DocumentHandler* weakSelf = self;
-    
+
     dispatch_queue_t asyncQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(asyncQueue, ^{
-        
+
         NSDictionary* dict = [command.arguments objectAtIndex:0];
-        
+
         NSString* urlStr = dict[@"url"];
         NSURL* url = [NSURL URLWithString:urlStr];
         NSData* dat = [NSData dataWithContentsOfURL:url];
@@ -26,17 +26,17 @@
         NSURL* tmpFileUrl = [[NSURL alloc] initFileURLWithPath:path];
         [dat writeToURL:tmpFileUrl atomically:YES];
         weakSelf.fileUrl = tmpFileUrl;
-        
+
         dispatch_async(dispatch_get_main_queue(), ^{
             QLPreviewController* cntr = [[QLPreviewController alloc] init];
             cntr.delegate = weakSelf;
             cntr.dataSource = weakSelf;
-            
+
             UIViewController* root = [[[UIApplication sharedApplication] keyWindow] rootViewController];
-            [root presentViewController:cntr animated:YES completion:nil];
+            [root presentViewController:cntr animated:NO completion:nil];
         });
-        
-        
+
+
         CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
         [weakSelf.commandDelegate sendPluginResult:commandResult callbackId:command.callbackId];
     });
